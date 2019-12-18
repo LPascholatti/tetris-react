c0d5d9196dcf41063d71673375558ea007065854
LPascholatti <lucaspascholatti@gmail.com>
2019-12-18 20:45:10 +0100

    Add game functions in the Tetris component and usePlayer useStage hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -5,18 +5,50 @@ import StartButton from './StartButton';
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { createStage } from '../gameHelpers';
 
 const Tetris = () => {
   
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
   console.log('re-render')
+
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0});
+  }
+
+  const startGame = () => {
+    // Reset Everything
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1)
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  }
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
       <StyledTetris>
       <Stage stage={stage}/>
       <aside>
@@ -28,7 +60,7 @@ const Tetris = () => {
           <Display text="Rows" />
           <Display text="Level" />
         </div>)}
-        <StartButton />
+        <StartButton onClick={startGame} />
       </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createStage } from '../gameHelpers';
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
 
   return [stage, setStage];
